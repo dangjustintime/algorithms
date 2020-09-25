e50d558d48868d7657f64e4e4102a32b008a49d5
dangjustintime <dangjustintime@gmail.com>
2020-09-25 15:12:57 -0700

    added mem size

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,8 @@
+import resource
+import timeit
+
 def helloWorld():
     print("Hello World")
 
-import timeit
-
-print("Run Time: " + str(timeit.timeit("helloWorld()", number=1, globals=globals())) + " seconds")
-
+print("Run Time:\t\t" + str(timeit.timeit("helloWorld()", number=1, globals=globals())) + " seconds")
+print("Memory Size:\t\t" +str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) + " bytes")
