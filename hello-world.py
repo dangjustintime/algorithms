ff30dd99ef2a054a4584fcd4f34476548a0a3e0a
dangjustintime <dangjustintime@gmail.com>
2020-09-25 15:25:27 -0700

    added measureAlgorithm function

--- hello-world.py
+++ hello-world.py
@@ -4,5 +4,11 @@ import timeit
 def helloWorld():
     print("Hello World")
 
-print("Run Time:\t\t" + str(timeit.timeit("helloWorld()", number=1, globals=globals())) + " seconds")
-print("Memory Size:\t\t" +str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) + " bytes")
+def measureAlgorithm(func):
+    print("\n------------------------------------------------------------\n")
+    print("Function:\t\t" + helloWorld.__name__ + "()")
+    print("Run Time:\t\t" + str(timeit.timeit(func.__name__, number=1, globals=globals())) + " seconds")
+    print("Memory Size:\t\t" +str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) + " bytes")
+    print("\n------------------------------------------------------------\n")
+
+measureAlgorithm(helloWorld)
